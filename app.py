e932137c9c9678d4946a0454609f472f0ba622e0
nishi267 <49039838+nishi267@users.noreply.github.com>
2020-09-14 15:17:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -73,10 +73,10 @@ def predict_note_file():
     clustr = KMeans(init='k-means++', n_clusters=5, n_init=10)
     clustr.fit(features)
     df_test['cluster_labels'] = clustr.labels_
-    str1=os.getcwd()
+    #str1=os.getcwd()
     df_test.to_csv(os.path.join(os.getcwd(), "test_cluster3.csv"))
     df_test.to_csv(r'C:/Users/Admin/Downloads/test_cluster3.csv')
-    return "Check the file is generated" + str1
+    return "Check the file is generated"
 
 
 if __name__ == '__main__':
